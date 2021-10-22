111ffffd3d96528d96d02b88f332ec2dda5a1b8b
Jethro Au <supergalong@gmail.com>
2021-10-22 03:11:54 +0100

    update project

--- src/components/ProjectsSection/data.js
+++ src/components/ProjectsSection/data.js
@@ -1,19 +1,11 @@
 const data = [
   {
-    image: require("../../assets/travel-advisor.png").default,
-    title: "Travel Advisor",
+    image: require("../../assets/netflix-clone.png").default,
+    title: "Netflix Clone",
     description:
-      "This project is built with ReactJS and MaterialUI. This project used Travel Advisor API, Google Map API and Places API.",
-    link: "https://mytravel-advisor.netlify.app/",
-    repo: "https://github.com/obiito01/travel-advisor",
-  },
-  {
-    image: require("../../assets/dolla.png").default,
-    title: "Dolla Website",
-    description:
-      "This website is built with ReactJS. React Router, styled-components and React Scroll are used in the project. This website is fully responsive.",
-    link: "https://dolla-vbanking.netlify.app/",
-    repo: "https://github.com/obiito01/dolla-website",
+      "This project is built with ReactJS. This project used The Movie Database (TMDB) API to fetch movie data. This website is deployed using Firebase.",
+    link: "https://netflix-clone-a5e1a.web.app/",
+    repo: "https://github.com/obiito01/netflix-clone",
   },
   {
     image: require("../../assets/ultra.png").default,
@@ -22,6 +14,14 @@ const data = [
       "This is Ultra website. This website is made with ReactJS, styled-components and React Router. This website is fully responsive!",
     link: "https://ultra-services.netlify.app/",
     repo: "https://github.com/obiito01/ultra-website",
+  },
+  {
+    image: require("../../assets/travel-advisor.png").default,
+    title: "Travel Advisor",
+    description:
+      "This project is built with ReactJS and MaterialUI. This project used Travel Advisor API, Google Map API and Places API.",
+    link: "https://mytravel-advisor.netlify.app/",
+    repo: "https://github.com/obiito01/travel-advisor",
   },
   {
     image: require("../../assets/crypto-tracker.png").default,
