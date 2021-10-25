01ea3d4d6dcc00faa3a6c3e1cb56683275664f65
Jethro Au <supergalong@gmail.com>
2021-10-25 23:57:53 +0100

    update data

--- src/components/ProjectsSection/data.js
+++ src/components/ProjectsSection/data.js
@@ -8,6 +8,14 @@ const data = [
     repo: "https://github.com/obiito01/netflix-clone",
   },
   {
+    image: require("../../assets/whatsapp-clone.png").default,
+    title: "WhatsApp Clone",
+    description:
+      "This is a WhatsApp clone web app made with React. You can send message in this app! Message data is stored in Firebase Firestore.",
+    link: "https://whatsapp-clone-d0564.web.app/rooms/HtwWPXyDqHIl8cE3yjtn",
+    repo: "https://github.com/obiito01/whatsapp-clone",
+  },
+  {
     image: require("../../assets/ultra.png").default,
     title: "Ultra Website",
     description:
@@ -22,14 +30,6 @@ const data = [
       "This project is built with ReactJS and MaterialUI. This project used Travel Advisor API, Google Map API and Places API.",
     link: "https://mytravel-advisor.netlify.app/",
     repo: "https://github.com/obiito01/travel-advisor",
-  },
-  {
-    image: require("../../assets/crypto-tracker.png").default,
-    title: "Crypto Tracker",
-    description:
-      "This is a Crypto Tracker web app made with React. You can track your favourite Crypto with the app! This app used CoinGecko API.",
-    link: "https://cryptotrkr.netlify.app",
-    repo: "https://github.com/obiito01/crypto-tracker",
   },
 ];
 
